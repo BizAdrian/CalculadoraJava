e037c0eed6bb2bd9508776483de8f8e9578a96f7
BizAdrian <medranoadrian645@gmail.com>
2023-05-21 19:19:31 -0400

    Primer commit

---
+++ src/TestCalculadora.java
@@ -0,0 +1,10 @@
+
+public class TestCalculadora {
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        System.out.println(calc.sumar(98, 457));
+        System.out.println(calc.restar(15, 10));
+        System.out.println(calc.multiplicar(14, 231));
+        System.out.println(calc.dividir(90, 10));
+    }
+}
